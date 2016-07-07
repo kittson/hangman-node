015df5091e80d1f35967f7932820b28bf4c471f6
kittson <gazorna@gmail.com>
2016-07-06 20:41:38 -0500

    pushing

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-var gameDictionary = ['jazz', 'about', 'men', 'work', 'farm'];
+
+function findTargetWord(dict) {
+		var lPos;
+		console.log( "dictionary is  " + dict ); 
+		var tWord = dict[Math.floor(Math.random() * dict.length)];
+		lPos = dict.indexOf(tWord);
+		dict.splice(lPos, 1);
+		
+		console.log( "executed findTargetWord  " + dict + " " + tWord);
+		return( tWord);
+}
+exports.findTargetWord = findTargetWord;
--- maingame.js
+++ maingame.js
@@ -1,11 +1,17 @@
-var dictionary = require('./game.js');
+var findTargetWord = require('./game.js').findTargetWord;
 var inquirer = require('inquirer');
 
-var gameDictionary = ['jazz', 'work', 'farm', 'hellfire'];
+var gameDictionary = ['jazz', 'workk', 'farrm', 'maan'];
+
+var wins = 0;
+var losses = 0;
+var workingWord ;
+var blanks = [];
+var guesses ;
 
 //find a random word in the dictionary, return that word and remove
 //the word from possible choices for the next play
-function findTargetWord(dict) {
+/*function findTargetWord(dict) {
 		var lPos;
 		console.log( "dictionary is  " + dict ); 
 		var tWord = dict[Math.floor(Math.random() * dict.length)];
@@ -14,25 +20,76 @@ function findTargetWord(dict) {
 		
 		console.log( "executed findTargetWord  " + dict + " " + tWord);
 		return( tWord);
+}*/
+
+//var workingWord = findTargetWord(gameDictionary).split(""); 
+
+
+function findAllLetters(guess){
+	var locationIndex = workingWord.indexOf(guess);
+	blanks[locationIndex] = guess;
+	workingWord = workingWord.replace(guess, "_");
+	console.log(blanks);
+	locationIndex = workingWord.indexOf(guess);
+	if (locationIndex > -1){
+		findAllLetters(guess);
+	};
+};
+
+function setNextWord(){
+	workingWord = findTargetWord(gameDictionary);	 
+	console.log("the word " + workingWord);
+	blanks = [];
+	guesses = workingWord.length;	
+	for ( i = 0; i < guesses; i++){
+		blanks.push("_");
+	};
 }
 
-var workingWord = findTargetWord(gameDictionary).split(""); 
-console.log( "splitted word " + workingWord);
 
 var askForGuess = function() {
-		var guesses = workingWord.length;
-		if (guesses > 0) {
-			inquirer.prompt([{
-				name: "guess",
-				message: "Guess a letter!"
-        	}]).then(function(answers) {
-				//do something
-				askForGuess();
-			})
-		}
-		else {
+	if( guesses === 0){
+		console.log("You lost!");
+		losses++;
+		doNewGame();
+		return;
+	}
 
-		}//else
+	if ( blanks.indexOf('_') > -1 ) {
+		inquirer.prompt([{
+			name: "guess",
+			message: "Guess a letter!"
+        }]).then(function(answers) {
+			var guess = answers.guess;
+			var locationIndex = workingWord.indexOf(guess);								
+			console.log(answers);
+			if (locationIndex > -1){
+				findAllLetters(guess);
+			}
+			else {
+				console.log("baad guess ");
+				console.log(blanks);
+				guesses--;
+			}
+			askForGuess();
+		})
+	}//if
+	else {
+		console.log("You won this word! Play again!");
+		wins++
+		doNewGame();
+	}		
 }//askForGuess function 
 
-askForGuess();
+function doNewGame(){
+	if( gameDictionary.length != 0 ){
+		setNextWord();
+		askForGuess();
+	}
+	else{
+		console.log("game over!");
+	}
+}
+
+
+doNewGame();
